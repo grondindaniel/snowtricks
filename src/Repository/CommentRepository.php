2995759ab3d44f6275070ef1fdd31d9b9a6266bd
Daniel Grondin <56930234+grondindaniel@users.noreply.github.com>
2020-06-27 16:07:31 +0200

    Update CommentRepository.php

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -19,22 +19,19 @@ class CommentRepository extends ServiceEntityRepository
         parent::__construct($registry, Comment::class);
     }
 
-    // /**
-    //  * @return Comment[] Returns an array of Comment objects
-    //  */
-    /*
+    /**
+     * @return Comment[] Returns an array of Comment objects
+     */
     public function findByExampleField($value)
     {
         return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
+            ->andWhere('c.trick = :val')
             ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
+            ->orderBy('c.created_at', 'DESC')
             ->getQuery()
             ->getResult()
         ;
     }
-    */
 
     /*
     public function findOneBySomeField($value): ?Comment
